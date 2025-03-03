fa36d9c214cb3eff3933faef5eb7ddbcb495fdef
sharon111111 <138508561+sharon111111@users.noreply.github.com>
2025-03-04 02:09:17 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,27 @@
+package com.example.tictactoe;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button[][] buttons = new Button[3][3];
+    private String currentPlayer = "X";
+    private TextView status;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        status = findViewById(R.id.status);
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button" + (i * 3 + j + 1);
+                int resID = getResources().
